c43cd10637abaab4c2ea89037109b36e3f115cfa
ChrisCarbonell <c_carbonell@yahoo.com>
2019-09-25 16:34:05 -0700

    merge in new problems

--- 1-dynamic-memory/src/course.cpp
+++ 1-dynamic-memory/src/course.cpp
@@ -9,7 +9,11 @@ Course::Course() {
     // Initialize member variables.
     // By default, course will hold a max of 25 students.
     // ========= ONLY MODIFY BETWEEN THE LINES  ===========
-
+    max_students = 25;
+    professor_cwid = -1;
+    class_num = -1;
+    num_students = 0;
+    students = new Student[25];
 
 
     // ====================================================
@@ -19,8 +23,11 @@ Course::Course(size_t max) {
     // Very similar to default constructor, but parameter specifies max
     // number of students.
     // ========= ONLY MODIFY BETWEEN THE LINES  ===========
-
-
+    professor_cwid = -1;
+    class_num = -1;
+    num_students = 0;
+    max_students = max;
+    students = new Student[max];
 
     // ====================================================
 }
@@ -28,7 +35,7 @@ Course::Course(size_t max) {
 Course::~Course() {
     // Utilized to free allocated memory.
     // ========= ONLY MODIFY BETWEEN THE LINES  ===========
-
+    delete [] students;
 
 
     // ====================================================
@@ -38,7 +45,9 @@ Student Course::get_student(size_t index) const {
     // Fetch student at specified index.
     // Utilize stdexcept to handle edge cases.
     // ========= ONLY MODIFY BETWEEN THE LINES  ===========
-    return Student();
+    if (index < 0 || index > max_students )
+      throw range_error("You're out of bounds. BOI!!!! Pick between 0-" + max_students);
+    return students[index];
 
 
     // ====================================================
@@ -48,6 +57,10 @@ void Course::enroll_student(const Student &student) {
     // Insert a new student
     // Utilize stdexcept to handle endge cases.
     // ========= ONLY MODIFY BETWEEN THE LINES  ===========
+    if( num_students >= max_students)
+      throw range_error("Number of students exceeds max student capacity.");
+    students[num_students] = student;
+    num_students++;
 
 
 
@@ -55,7 +68,7 @@ void Course::enroll_student(const Student &student) {
 }
 
 
-void Course::set_class_number(int num) { 
+void Course::set_class_number(int num) {
     class_num = num;
 }
 
@@ -78,4 +91,3 @@ size_t Course::get_num_students() const {
 int Course::get_professor_cwid() const {
     return professor_cwid;
 }
-
